fab4378d35c3e6f1523992c717415f457d27dfb8
Vasily Novak <novak-vasil@yandex.ru>
2020-02-26 11:09:40 +0300

    Added css to some files

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.views.generic.base import View
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseBadRequest, Http404, HttpResponse, HttpResponseForbidden
-from django.shortcuts import render, get_object_or_404, redirect, render_to_response
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.csrf import csrf_protect
 from django.utils.decorators import method_decorator
 
