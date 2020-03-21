6106b635235263e43f8dd2911329597cb9d4bf36
Vasily Novak <47313974+dfczyjd@users.noreply.github.com>
2020-03-21 13:51:23 +0300

    Resolve conflicts

--- LegalDD/settings.py
+++ LegalDD/settings.py
@@ -119,5 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'LegalDD', 'static', 'static_dirs')
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'LegalDD', 'static', 'static_root')
 
-MEDIA_ROOT = BASE_DIR + '/documents/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'LegalDD', 'documents')
