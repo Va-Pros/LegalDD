dd6666c01b8a11a42356a47a5783c73e42e72881
dfczyjd <novak-vasil@yandex.ru>
2020-04-05 17:36:09 +0300

    Fix tests + add url to logger

--- main/logger.py
+++ main/logger.py
@@ -2,6 +2,7 @@ from sys import stderr
 
 def log_get_params(get_view):
     def wrapper(request):
+        print("URL:", request.build_absolute_uri('?'))
         print("GET request params:", file=stderr)
         for key, value in request.GET.items():
             print(key + ': ' + value, file=stderr)
@@ -12,6 +13,7 @@ def log_get_params(get_view):
 
 def log_post_params(post_view):
     def wrapper(request):
+        print("URL:", request.build_absolute_uri('?'))
         print("POST request params:", file=stderr)
         for key, value in request.POST.items():
             print(key + ': ' + value, file=stderr)
